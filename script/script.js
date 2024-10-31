be9b9cef45884f5a7761954dfa1b067649cea131
Kaique <kaique.rodrigues328328a@gmail.com>
2024-10-31 11:30:58 -0300

    summit

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
+    let icon = document.querySelector('.mobile-menu-icon .icon');
+
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+        icon.src = "assets/img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+        icon.src = "assets/img/close_white_36dp.svg";
     }
 }
+
+// Adiciona o evento ao botão do sidebar
+document.getElementById('open_btn').addEventListener('click', function () {
+    let sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open-sidebar');
+});
