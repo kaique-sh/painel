4b5728f948b28df41c2b0da0e95390b1920e2e92
Kaique <kaique.rodrigues328328a@gmail.com>
2024-11-01 13:21:48 -0300

    atualizado

--- script/script.js
+++ script/script.js
@@ -2,17 +2,29 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     let icon = document.querySelector('.mobile-menu-icon .icon');
 
+    // Verifica se o menu está aberto
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        icon.src = "assets/img/menu_white_36dp.svg";
+        icon.src = "assets/img/menu_white_36dp.svg"; // Ícone de menu
     } else {
         menuMobile.classList.add('open');
-        icon.src = "assets/img/close_white_36dp.svg";
+        icon.src = "assets/img/close_white_36dp.svg"; // Ícone de fechar
+    }
+}
+
+function toggleSidebar() {
+    let sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open-sidebar'); // Alterna a classe da sidebar
+    // Alterna ícone do botão
+    let icon = document.getElementById('open_btn').querySelector('i');
+    if (sidebar.classList.contains('open-sidebar')) {
+        icon.classList.remove('bi-arrow-left-circle-fill');
+        icon.classList.add('bi-arrow-right-circle-fill'); // Exibe o ícone de fechar
+    } else {
+        icon.classList.remove('bi-arrow-right-circle-fill');
+        icon.classList.add('bi-arrow-left-circle-fill'); // Exibe o ícone de menu
     }
 }
 
 // Adiciona o evento ao botão do sidebar
-document.getElementById('open_btn').addEventListener('click', function () {
-    let sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('open-sidebar');
-});
+document.getElementById('open_btn').addEventListener('click', toggleSidebar);
